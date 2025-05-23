57dc52f3800d46bf0cd13ce61f5a558b489cbabe
saranggore11 <159864295+saranggore11@users.noreply.github.com>
2025-05-22 22:36:43 -0400

    Commented EC2 instance section

--- main.tf
+++ main.tf
@@ -13,11 +13,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
+/*resource "aws_instance" "app_server" {
   ami           = "ami-0e58b56aa4d64231b"
   instance_type = "t2.micro"
 
   tags = {
     Name = "EC2InstanceCreatedByTerraform"
   }
-}
+}*/
+
+
